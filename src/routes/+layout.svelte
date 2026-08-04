0ad3b4507c4b11d98a18131b127c368229077b0a
Gavin <gavin@fallingonline.com>
2026-08-04 00:03:22 -0500

    Nav bar initial setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,15 @@
 	const { children } = $props();
 </script>
 
+<div class="w-full h-[66px] px-4">
+    <img class="logo" src="/logo_clear.png" alt="Imagine Display Logo" />
+</div>
+
 {@render children()}
+
+<style>
+    .logo {
+        height: 100%;
+    }
+</style>
+
